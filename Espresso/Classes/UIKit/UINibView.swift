7d5616c40641e749fc1168b394f028270abe004c
Mitch Treece <mitchtreece@me.com>
2018-07-05 21:31:32 -0700

    Added UINibView

---
+++ Espresso/Classes/UIKit/UINibView.swift
@@ -0,0 +1,25 @@
+//
+//  UINibView.swift
+//  Espresso
+//
+//  Created by Mitch Treece on 7/5/18.
+//
+
+import UIKit
+
+/**
+ `UIView` subclass that loads it's contents from a nib.
+ */
+public class UINibView: UIView {
+    
+    /**
+     Initializes a new instance of the view from a nib. If no name is provided, the class name will be used.
+     
+     - Parameter name: The nib's name.
+     - Returns: A typed nib-loaded view instance.
+     */
+    public static func initFromNib(named name: String? = nil) -> Self {
+        return self.loadFromNib(name: name)
+    }
+    
+}
