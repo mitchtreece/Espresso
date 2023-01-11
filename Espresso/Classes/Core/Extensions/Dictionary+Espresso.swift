5ab24ac6e2184cfce49760406aab037f7d3eba68
Mitch Treece <mitchtreece@me.com>
2023-01-11 14:54:13 -0800

    Removed json-specific compact() helper. Not needed.

--- Espresso/Classes/Core/Extensions/Dictionary+Espresso.swift
+++ Espresso/Classes/Core/Extensions/Dictionary+Espresso.swift
@@ -25,26 +25,3 @@ public extension Dictionary where Value: OptionalType {
     }
     
 }
-
-// NOTE: There is some weirdness when trying to compact JSON types
-// that have nested JSON objects. Providing a more explicit extension
-// to resolve these issues.
-
-extension Dictionary where Key == JSON.Key, Value == JSON.Value? {
-
-    /// Creates a new json object by removing key/value pairs with `nil` values.
-    /// - returns: A new concrete-value json object.
-    ///
-    ///     let json: [String: Any?] = [
-    ///         "one": 1,
-    ///         "two": nil,
-    ///         "three": 3
-    ///     ]
-    ///
-    ///     let compactJson: JSON = json.compact()
-    ///     print(compactJson) => "["one": 1, "three": 3]"
-    func compact() -> JSON {
-        compactMapValues { $0 }
-    }
-
-}
