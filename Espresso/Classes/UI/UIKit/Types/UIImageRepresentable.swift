9a57465111146e94a8ca2256c483023cb071cd6a
Mitch Treece <mitchtreece@me.com>
2023-01-12 14:01:22 -0800

    Finished renaming ui classes

--- Espresso/Classes/UI/UIKit/Types/UIImageRepresentable.swift
+++ Espresso/Classes/UI/UIKit/Types/UIImageRepresentable.swift
@@ -1,5 +1,5 @@
 //
-//  ImageType.swift
+//  UIImageRepresentable.swift
 //  Espresso
 //
 //  Created by Mitch Treece on 5/3/21.
@@ -7,8 +7,8 @@
 
 import UIKit
 
-/// Protocol describing something that can be represented as an image.
-public protocol ImageRepresentable {
+/// Protocol describing something that can be represented as a `UIImage`.
+public protocol UIImageRepresentable {
     
     /// An image representation.
     /// - returns: An image.
@@ -16,18 +16,24 @@ public protocol ImageRepresentable {
     
 }
 
-public extension ImageRepresentable {
+public extension UIImageRepresentable {
     
     /// An image representation using a given rendering mode.
     /// - parameter renderingMode: The image rendering mode to use.
-    /// - returns: An image using the give rendering mode.
+    /// - returns: An image.
     func asImage(renderingMode: UIImage.RenderingMode) -> UIImage? {
         return asImage()?.withRenderingMode(renderingMode)
     }
     
+    /// An image data representation.
+    /// - returns: Image data.
+    func asImageData() -> Data? {
+        return asImage()?.pngData()
+    }
+    
 }
 
-extension UIImage: ImageRepresentable {
+extension UIImage: UIImageRepresentable {
     
     public func asImage() -> UIImage? {
         return self
@@ -35,7 +41,7 @@ extension UIImage: ImageRepresentable {
     
 }
 
-extension Data: ImageRepresentable {
+extension Data: UIImageRepresentable {
     
     public func asImage() -> UIImage? {
         return UIImage(data: self)
