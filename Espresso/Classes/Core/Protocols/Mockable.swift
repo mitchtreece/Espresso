53f584159754e98217b499884b71aed0221275ed
Mitch Treece <mitchtreece@me.com>
2023-02-14 13:54:00 -0800

    Updated Mockable protocol

--- Espresso/Classes/Core/Protocols/Mockable.swift
+++ Espresso/Classes/Core/Protocols/Mockable.swift
@@ -10,18 +10,26 @@ import Foundation
 /// Protocol that describes something that can be represented using mock data.
 public protocol Mockable {
 
-    /// Creates a mock of this type.
+    /// Creates a mock value.
     static func mock() -> Self
     
-    /// Creates a mock-array of this type.
+    /// Creates a specified number of mock values.
+    /// - parameter count: The requested number of mock values.
+    /// - returns: An array of mock values.
     static func mocks(count: Int) -> [Self]
 
 }
 
 public extension Mockable {
     
+    /// Creates an array of mock values.
+    /// - returns: An array of mock values.
+    ///
+    /// By default this requests an array of 5 mock values.
+    ///
+    /// If you'd like a different number of values, use `mocks(count:)`
     static func mocks() -> [Self] {
-        return mocks(count: 3)
+        return mocks(count: 5)
     }
     
 }
