9a0776f9778f1f1b7efd1f4c98298e3e890ebfe0
Mitch Treece <mitchtreece@me.com>
2023-04-13 15:15:31 -0700

    Moved more of the string extensions to uikit module

--- Sources/Core/Extensions/String+Espresso.swift
+++ Sources/Core/Extensions/String+Espresso.swift
@@ -30,57 +30,6 @@ public extension Optional where Wrapped == String {
     
 }
 
-public extension String /* Size */ {
-    
-    /// Calculates the display size for the string using a constraint & attributes.
-    /// - Parameter size: The constrained size.
-    /// - Parameter attributes: The string attributes to use while calculating the size.
-    /// - Returns: The string's display size.
-    func size(constrainedTo size: CGSize,
-              attributes: [NSAttributedString.Key: Any]?) -> CGSize {
-        
-        return (self as NSString).boundingRect(
-            with: size,
-            options: [.usesLineFragmentOrigin],
-            attributes: attributes,
-            context: nil
-        )
-        .size
-        
-    }
-    
-    /// Calculates the display width for the string using a constrained height & attributes.
-    /// - Parameter height: The constrained height.
-    /// - Parameter attributes: The string attributes to use while calculating the width.
-    /// - Returns: The string's display width.
-    func width(forHeight height: CGFloat,
-               attributes: [NSAttributedString.Key: Any]?) -> CGFloat {
-        
-        return size(
-            constrainedTo: CGSize(width: CGFloat.greatestFiniteMagnitude, height: height),
-            attributes: attributes
-        )
-        .width
-        
-    }
-    
-    /// Calculates the display height for the string using a constrained width & attributes.
-    /// - Parameter width: The constrained width.
-    /// - Parameter attributes: The string attributes to use while calculating the height.
-    /// - Returns: The string's display height.
-    func height(forWidth width: CGFloat,
-                attributes: [NSAttributedString.Key: Any]?) -> CGFloat {
-        
-        return size(
-            constrainedTo: CGSize(width: width, height: CGFloat.greatestFiniteMagnitude),
-            attributes: attributes
-        )
-        .height
-        
-    }
-    
-}
-
 public extension String /* Tokens */ {
     
     /// Creates a new string by replacing all occurrences of a **\<token\>** with another string.
