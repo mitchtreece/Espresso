804b1f206f09fa32e2b5052b55032ecf163e719f
Mitch Treece <mitchtreece@me.com>
2023-04-13 16:17:30 -0700

    Removed redundent modelId var on UIDevice extension

--- Sources/UI/UIKit/Extensions/UIDevice+Espresso.swift
+++ Sources/UI/UIKit/Extensions/UIDevice+Espresso.swift
@@ -9,17 +9,7 @@ import UIKit
 import Espresso // Ignore warning, we need this for SPM modules
 
 public extension UIDevice {
-    
-    /// The current device's model identifier.
-    ///
-    /// i.e. "iPhone1,1", "iPhone10,3", "iPhone14,3"
-    var modelIdentifier: String {
-        
-        return AppleDevice
-            .modelIdentifier(simulatorPrefix: false)
-        
-    }
-    
+
     /// An object that represents the current Apple device.
     static var currentAppleDevice: AppleDevice {
         return AppleDevice.current
