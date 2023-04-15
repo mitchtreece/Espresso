6dbaf28173c6eb470410d11f8b27b45e7dcf9e80
Mitch Treece <mitchtreece@me.com>
2023-04-14 20:29:54 -0700

    Submodules now implicitly export Espresso

--- Sources/Promise/_Exports.swift
+++ Sources/Promise/_Exports.swift
@@ -1,1 +1,2 @@
 @_exported import PromiseKit
+@_exported import Espresso
--- Sources/UI/_Exports.swift
+++ Sources/UI/_Exports.swift
@@ -2,3 +2,4 @@
 @_exported import SwiftUI
 @_exported import SnapKit
 @_exported import Kingfisher
+@_exported import Espresso
