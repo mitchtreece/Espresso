1f7e8453aa96f2b782d93504266e51ff5e4823f5
Mitch Treece <mitchtreece@me.com>
2023-12-10 15:56:36 -0800

    Added UIColor.asSwiftColor()

---
+++ Sources/UI/SwiftUI/Extensions/UIColor+SwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  Binding+Espresso.swift
+//  Espresso
+//
+//  Created by Mitch Treece on 12/10/23.
+//
+
+import UIKit
+import SwiftUI
+
+public extension UIColor {
+    
+    /// A SwiftUI `Color` representation.
+    func asSwiftColor() -> Color {
+        return Color(self)
+    }
+    
+}
