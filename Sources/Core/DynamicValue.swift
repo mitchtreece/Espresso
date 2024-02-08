7e1d6b91023bc4a08afcc19679e1e829556d8929
Mitch Treece <mitchtreece@me.com>
2024-02-08 13:59:20 -0800

    Added DynamicValue documentation

--- Sources/Core/DynamicValue.swift
+++ Sources/Core/DynamicValue.swift
@@ -7,12 +7,25 @@
 
 import Foundation
 
+/// A dynamic value wrapper that represents potential
+/// concrete & placeholder values, as-well-as a default fallback value.
 public struct DynamicValue<T> {
     
+    /// The current value.
     public var value: T?
+    
+    /// the placeholder value.
     public let placeholder: T?
+    
+    /// The default value.
     public let `default`: T
     
+    /// Initializes a dynamic value with a
+    /// current value, placeholder value, & default value.
+    ///
+    /// - parameter value: The current value.
+    /// - parameter placeholder: The placeholder value.
+    /// - parameter default: The default value.
     public init(value: T? = nil,
                 placeholder: T? = nil,
                 `default`: T) {
@@ -23,6 +36,10 @@ public struct DynamicValue<T> {
         
     }
     
+    /// Gets the current/default, or a placeholder value.
+    ///
+    /// - parameter placeholder: Flag indicating if a placeholder value should be returned.
+    /// - returns: A current/default _or_ placeholder value.
     public func value(placeholder: Bool = false) -> T {
         
         return placeholder ?
