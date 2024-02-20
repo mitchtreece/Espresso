710c26a39fe1e7522f84a2dcdfa9c7e7b5c8faa0
Mitch Treece <mitchtreece@me.com>
2024-02-19 17:20:10 -0800

    Moved Dictionary.compact extension to its own file

--- Sources/Core/Extensions/Dictionary+Espresso.swift
+++ Sources/Core/Extensions/Dictionary+Espresso.swift
@@ -7,25 +7,6 @@
 
 import Foundation
 
-public extension Dictionary where Value: OptionalType /* Compact */ {
-    
-    /// Creates a new dictionary by removing key/value pairs with `nil` values.
-    /// - returns: A new concrete-value dictionary.
-    ///
-    ///     let dictionary: [String: Int?] = [
-    ///         "one": 1,
-    ///         "two": nil,
-    ///         "three": 3
-    ///     ]
-    ///
-    ///     let compactDictionary: [String: Int] = dictionary.compact()
-    ///     print(compactDictionary) => "["one": 1, "three": 3]"
-    func compact() -> [Key: Value.Wrapped] {
-        return compactMapValues { $0 as? Value.Wrapped }
-    }
-    
-}
-
 public extension Dictionary /* Merge */ {
     
     /// Creates a dictionary by merging the given dictionary
