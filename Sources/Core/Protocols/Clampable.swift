30806ab43471bbda436ee9f0682a4a124cc8cf58
Mitch Treece <mitchtreece@me.com>
2025-01-15 12:49:25 -0800

    Added range support to clampable

--- Sources/Core/Protocols/Clampable.swift
+++ Sources/Core/Protocols/Clampable.swift
@@ -7,34 +7,79 @@
 
 import Foundation
 
-/// Protocol describing the attributes of something that can be clamped.
-public protocol Clampable {
+/// Clamps the receiver from a minimum, through a maximum value.
+/// - parameter value: The value to clamp.
+/// - parameter min: The minimum value.
+/// - parameter max: The maximum value.
+/// - returns: The clamped value.
+public func clamped<T: Clampable>(_ value: T, min: T, max: T) -> T {
     
-    /// Clamps between minimum & maximum values.
-    /// - Parameter min: The minimum value.
-    /// - Parameter max: The maximum value.
-    /// - Returns: The clamped value.
-    func clamp(min: Self, max: Self) -> Self
-    
-}
-
-/// Clamps a value between minimum & maximum values.
-/// - Parameter value: The value to clamp.
-/// - Parameter min: The minimum value.
-/// - Parameter max: The maximum value.
-/// - Returns: The clamped value.
-public func clamp<T: Clampable>(_ value: T, min: T, max: T) -> T {
-    
-    return value.clamp(
+    return value.clamped(
         min: min,
         max: max
     )
     
 }
 
+/// Clamps the receiver from minimum, up-to maximum values in a range.
+/// - parameter value: The value to clamp.
+/// - parameter range: The range to clamp the value within.
+/// - returns: The clamped value.
+public func clamped<T: Clampable>(_ value: T, range: Range<T>) -> T {
+    return value.clamped(range)
+}
+
+/// Clamps the receiver from minimum, through maximum values in a range.
+/// - parameter value: The value to clamp.
+/// - parameter range: The range to clamp the value through.
+/// - returns: The clamped value.
+public func clamped<T: Clampable>(_ value: T, range: ClosedRange<T>) -> T {
+    return value.clamped(range)
+}
+
+/// Protocol describing the attributes of something
+/// that can be clamped to / within a range.
+public protocol Clampable: Comparable {
+    
+    /// Clamps the receiver from a minimum, through a maximum value.
+    /// - parameter min: The minimum value.
+    /// - parameter max: The maximum value.
+    /// - returns: The clamped value.
+    func clamped(min: Self, max: Self) -> Self
+    
+}
+
+public extension Clampable {
+    
+    /// Clamps the receiver from minimum, up-to maximum values in a range.
+    /// - parameter range: The range to clamp the value within.
+    /// - returns: The clamped value.
+    func clamped(_ range: Range<Self>) -> Self {
+        
+        return clamped(
+            min: range.lowerBound,
+            max: range.upperBound
+        )
+        
+    }
+    
+    /// Clamps the receiver from minimum, through maximum values in a range.
+    /// - parameter range: The range to clamp the value through.
+    /// - returns: The clamped value.
+    func clamped(_ range: ClosedRange<Self>) -> Self {
+        
+        return clamped(
+            min: range.lowerBound,
+            max: range.upperBound
+        )
+        
+    }
+    
+}
+
 extension Double: Clampable {
 
-    public func clamp(min: Double, max: Double) -> Double {
+    public func clamped(min: Double, max: Double) -> Double {
         return Swift.min(max, Swift.max(min, self))
     }
 
@@ -42,7 +87,7 @@ extension Double: Clampable {
 
 extension Float: Clampable {
 
-    public func clamp(min: Float, max: Float) -> Float {
+    public func clamped(min: Float, max: Float) -> Float {
         return Swift.min(max, Swift.max(min, self))
     }
 
@@ -50,7 +95,7 @@ extension Float: Clampable {
 
 extension CGFloat: Clampable {
 
-    public func clamp(min: CGFloat, max: CGFloat) -> CGFloat {
+    public func clamped(min: CGFloat, max: CGFloat) -> CGFloat {
         return Swift.min(max, Swift.max(min, self))
     }
 
@@ -58,7 +103,7 @@ extension CGFloat: Clampable {
 
 extension Int: Clampable {
     
-    public func clamp(min: Int, max: Int) -> Int {
+    public func clamped(min: Int, max: Int) -> Int {
         return Swift.min(max, Swift.max(min, self))
     }
 
